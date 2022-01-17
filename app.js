148df70d9c49d10af5eca0e6f8975f0ce8015dcf
Sanchayan <sanchayan.sarkar1997@gmail.com>
2022-01-17 23:22:53 +0530

    Comments added for future reference

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function sayHello(name){
 console.log('Hello'+name);
 }
-console.log(window);
-//sayHello('Mosh');
+// console.log(window); This statement if uncommented, will throw an exception: window is not definded; this is becasue in Node we donot have window/document objects. They are part of the runtime environment that we get with browsers
+sayHello('Sanchayan');
